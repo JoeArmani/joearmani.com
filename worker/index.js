3bc3c836f226c78b65a17bf90af5d8da46e8926b
Joe Armani <jvarmani@gmail.com>
2026-05-27 08:43:54 -0600

    link canonicalization

--- worker/index.js
+++ worker/index.js
@@ -1,8 +1,10 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
+    const isSiteHostname = url.hostname === 'joearmani.com' || url.hostname === 'www.joearmani.com';
 
-    if (url.hostname === 'www.joearmani.com') {
+    if (isSiteHostname && (url.protocol !== 'https:' || url.hostname === 'www.joearmani.com')) {
+      url.protocol = 'https:';
       url.hostname = 'joearmani.com';
       return Response.redirect(url.toString(), 308);
     }
